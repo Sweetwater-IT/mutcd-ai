707f4b6ce2630c3c6b605ad136cc621830cc95bc
Kenny Mack <knnymck@icloud.com>
2025-10-21 20:50:18 -0500

    Typecast PDFViewer import to any

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ interface PDFWithSigns {
   selectedPage: number
 }
 
-const PDFViewer = dynamic(() => import("@/components/pdf-viewer").then(mod => mod.PDFViewer), {
+const PDFViewer = dynamic(() => import("@/components/pdf-viewer").then((mod) => mod.PDFViewer as any), {
   ssr: false,
   loading: () => (
     <div className="flex h-[calc(100vh-12rem)] items-center justify-center rounded-lg border border-border bg-card">
